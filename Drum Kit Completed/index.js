8c72a72c4bbc0d5e3f5c5d5f9eca5c40ba5788f7
Muhd Rifat Uddin <110630361+Rifat2314@users.noreply.github.com>
2024-03-07 01:19:37 +0600

    Update index.js
    change to a b c d e f g

--- Drum Kit Completed/index.js
+++ Drum Kit Completed/index.js
@@ -21,17 +21,17 @@ document.addEventListener("keypress", function(event) {
 function makeSound(key) {
 
   switch (key) {
-    case "w":
+    case "a":
       var tom1 = new Audio("sounds/tom-1.mp3");
       tom1.play();
       break;
 
-    case "a":
+    case "b":
       var tom2 = new Audio("sounds/tom-2.mp3");
       tom2.play();
       break;
 
-    case "s":
+    case "c":
       var tom3 = new Audio('sounds/tom-3.mp3');
       tom3.play();
       break;
@@ -41,17 +41,17 @@ function makeSound(key) {
       tom4.play();
       break;
 
-    case "j":
+    case "e":
       var snare = new Audio('sounds/snare.mp3');
       snare.play();
       break;
 
-    case "k":
+    case "f":
       var crash = new Audio('sounds/crash.mp3');
       crash.play();
       break;
 
-    case "l":
+    case "g":
       var kick = new Audio('sounds/kick-bass.mp3');
       kick.play();
       break;
